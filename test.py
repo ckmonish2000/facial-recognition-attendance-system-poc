8fc74d829a35591ae9ec39427aeaf23d006c0952
monish karunakaran <ckmonish2000@gmail.com>
2021-09-01 17:06:11 +0530

    minor

--- test.py
+++ test.py
@@ -12,9 +12,17 @@ img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
 
 # find face loc and convert img to convolution
 faceLoc = face_recognition.face_locations(img1)[0]
-encode1 = face_recognition.face_encodings(img1)
+encode1 = face_recognition.face_encodings(img1)[0]
 cv2.rectangle(img1,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
 
+faceLoc2 = face_recognition.face_locations(img2)[0]
+encode2 = face_recognition.face_encodings(img2)[0]
+cv2.rectangle(img2,(faceLoc2[3],faceLoc2[0]),(faceLoc2[1],faceLoc2[2]),(255,0,255),2)
+
+
+result = face_recognition.compare_faces([encode1],encode2)
+face_distance = face_recognition.face_distance([encode1],encode2)
+cv2.putText(img2,f'{result} {round(face_distance[0],2)}',(90,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
 
 
 cv2.imshow("img 1",img1)
